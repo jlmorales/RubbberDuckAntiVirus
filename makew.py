c826ffd883786bd6d7503677753e3cf892838bb2
jlmorales <38224764+jlmorales@users.noreply.github.com>
2018-11-25 21:21:16 -0500

    added scripts to make whitelist

---
+++ makew.py
@@ -0,0 +1,16 @@
+import hashlib, os, sys
+
+f = open("whitelist.txt", "w")
+
+for root, dirs,files in os.walk('/usr/bin', topdown=True):
+    for name in files:
+        #print(os.path.join(root, name))
+        FileName = (os.path.join(root, name))
+
+        hasher = hashlib.sha1()
+        with open(str(FileName), 'rb') as afile:
+            buf = afile.read()
+            hasher.update(buf)
+        print(hasher.hexdigest())
+        f.write(hasher.hexdigest())
+        #f.write("")
