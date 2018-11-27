c9915466618b0a01f938376ca290304147543c17
SpencerX <spencer.xia@stonybrook.edu>
2018-11-27 16:38:24 -0500

    update embedded python

--- update.c
+++ update.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <python3.5/Python.h>
 
 #include "update.h"
 
@@ -9,6 +10,11 @@ FILE *listfd;
 void update()
 {
     printf("Update func reached");
-    listfd = fopen("whitelist/whitelist.txt", "w+");
-
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import requests");
+    PyRun_SimpleString("sys.path.append(\".\")");
+    PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+    PyRun_SimpleString("import update");
+    Py_Finalize();
 }
