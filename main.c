571d7e833e63be885209d78533fc15eeed21c74f
SpencerX <Spencer.Xia@stonybrook.edu>
2018-12-01 08:13:41 -0800

    main changed to fit kernel module

--- main.c
+++ main.c
@@ -2,37 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <syslog.h>
 
 #include "update.h"
 #include "iterator.h"
 
 int main(int argc, char **argv){
-
     int arg;
     char *pathname = NULL;
+    openlog("vyatta-conntrack", LOG_PID, LOG_USER);
+    syslog(LOG_ALERT, "Mid logging");
+    syslog(LOG_ALERT, "arg: %s", argv[1]);
+    if (argv[1] != NULL)
+    {
+        if (strcmp(argv[1], "-a") == 0)
+        {
+                //iterator("/home/student/Desktop");
+                //readbytes("/home/jose/Documents/cse331project/whitelist/whitelist.txt");
+                findInWhite("/usr/bin/[");
+                if (argv[2] != NULL)
+                {
+                pathname = optarg;
+                syslog(LOG_ALERT, "preprintf logging");
+                printf("%s\n", pathname);
+                syslog(LOG_ALERT, "postprintf logging");
+                iterator(pathname);
+                }
+        }
+        else if (strcmp(argv[1], "-u") == 0)
+        {                
+                printf("Update func\n");
+                syslog(LOG_ALERT, "Update logging");
+                update();                   
+        }
+        else if (strcmp(argv[1], "-o") == 0)
+        {
+                printf("On-Access mode");
+                syslog(LOG_ALERT, "onaccess logging");
 
-    while ((arg = getopt (argc, argv, "a:uo")) != -1)
-        switch (arg)
-            {
-                case 'a':
-                    //iterator("/home/student/Desktop");
-                    //readbytes("/home/jose/Documents/cse331project/whitelist/whitelist.txt");
-                    findInWhite("/usr/bin/[");
-                    pathname = optarg;
-                    printf("%s\n", pathname);
-                    iterator(pathname);
-                    break;
-                case 'u':
-                    printf("Update func\n");
-                    update();
-                    break;
-                case 'o':
-                    printf("On-Access mode");
-                    break;
-                default:
-                    printf("Arguments:\n");
-                    printf("On-Demand Scan: -a (pathname)\n");
-                    printf("Update: -u\n");
-            }
-
+        }
+        else
+        {
+                printf("Arguments:\n");
+                printf("On-Demand Scan: -a (pathname)\n");
+                printf("Update: -u\n");
+        }
+    }
+    closelog();   
 }
